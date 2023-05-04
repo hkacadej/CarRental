91483469b7a1d8c91cc307ab4e17edfc9942a32d
Anixher <rexhinaalleshi@gmail.com>
2023-05-04 17:34:07 +0200

    Admin/User Roles (unfinished)

---
+++ backend/src/main/java/TheCarRentalProject/Configuration/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package TheCarRentalProject.Configuration;
+
+import TheCarRentalProject.Service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE)
+                .hasRole("ADMIN")
+                .antMatchers("/admin/**")
+                .hasAnyRole("ADMIN")
+                .antMatchers("/user/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/login/**")
+                .anonymous()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+}
---
+++ backend/src/main/java/TheCarRentalProject/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package TheCarRentalProject.Repository;
+
+import TheCarRentalProject.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
